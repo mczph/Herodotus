d3f496503d9ea91f70cd014bd19f0166f47e3259
youyihj <youyi580@qq.com>
2021-03-15 08:04:39 +0800

    update

--- ftbq_lang_processor.py
+++ ftbq_lang_processor.py
@@ -1,6 +1,4 @@
 import os
-import json
-import re
 
 ftbquests_path = ".minecraft/config/ftbquests"
 zh_cn_path = ".minecraft/resources/herodotus/lang/zh_cn.lang"
@@ -9,8 +7,7 @@ should_replace_key = [
     "title",
     "description"
 ]
-cn_context_dict = {}
-en_context_dict = {}
+context_dict = {}
 
 def check_dir(path):
     for file in os.listdir(path):
@@ -25,21 +22,23 @@ def read_snbt(full_path, file_name):
     f_list = f.readlines()
     file_name = file_name.split(".snbt")[0]
     f.close()
+    if (file_name == "chapter"):
+        file_name = full_path.split("/")[-2]
     for i, line in enumerate(f_list):
         for key in should_replace_key:
-            if (key in line):
-                context = line.split("\"")
-                lang_key = "herodotus.quests.%s.%s" % (file_name, key)
-                new_context = context[0] + "\"{" + lang_key + "}\"" + context[2]
-                f_list[i] = new_context
-                cn_context_dict[lang_key] = context[1]
-                en_context_dict[lang_key] = context[1]
+            if (line.lstrip().startswith(key)):
+                head, context, tail = line.split("\"")
+                if not(context.startswith("{") and context.endswith("}")):
+                    lang_key = "herodotus.quests.%s.%s" % (file_name, key)
+                    new_context = head + "\"{" + lang_key + "}\"" + tail
+                    f_list[i] = new_context
+                    context_dict[lang_key] = context
     f = open(full_path, "w+", encoding="utf-8")
     f.writelines(f_list)
     f.close()
 
-def write_lang(path, context_dict):
-    new_file(path)
+def write_lang(path):
+    copy = context_dict.copy()
     to_append_entries = []
     f = open(path, "r+", encoding='utf-8')
     f_list = f.readlines()
@@ -49,20 +48,18 @@ def write_lang(path, context_dict):
         if (line.startswith("#")):
             continue
         key = line.split("=")[0]
-        if (key in context_dict):
-            f_list_copy[i] = context_dict.pop(key)
-    for key, value in context_dict.items():
+        if (key in copy):
+            f_list_copy[i] = key + "=" + copy.pop(key)
+    for key, value in copy.items():
         to_append_entries.append(key + "=" + value)
     f = open(path, "w+", encoding="utf-8")
     f.writelines(f_list_copy)
+    if (len(f_list[-1]) == 0):
+        f.write("\n")
     for entry in to_append_entries:
         f.write(entry + "\n")
     f.close()
 
-def new_file(path):
-    f = open(path, "w", encoding="utf-8")
-    f.close()
-
 check_dir(ftbquests_path)
-write_lang(zh_cn_path, cn_context_dict)
-write_lang(en_us_path, en_context_dict)
+write_lang(zh_cn_path)
+write_lang(en_us_path)
