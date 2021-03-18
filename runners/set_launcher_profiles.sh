7c9bf2ef9657209228a5801e94899e6231e0da57
youyihj <30400991+friendlyhj@users.noreply.github.com>
2021-03-19 00:36:22 +0800

    Update set_launcher_profiles.sh

--- runners/set_launcher_profiles.sh
+++ runners/set_launcher_profiles.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -x
 
 filePath="${GITHUB_WORKSPACE}/gml/.minecraft/launcher_profiles.json"
-echo "{\"selectedProfile\": \"(Default)\",\"profiles\": {\"(Default)\": {\"name\": \"(Default)\"}},\"clientToken\": \"88888888-8888-8888-8888-888888888888\"}" > ${filePath}
+cat "{\"selectedProfile\": \"(Default)\",\"profiles\": {\"(Default)\": {\"name\": \"(Default)\"}},\"clientToken\": \"88888888-8888-8888-8888-888888888888\"}" > ${filePath}
