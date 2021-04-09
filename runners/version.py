63e83feafedd7c89e7171801757653e03885a6ef
youyihj <youyi580@qq.com>
2021-04-09 17:06:55 +0800

    fix

--- runners/version.py
+++ runners/version.py
@@ -1,12 +1,12 @@
 import os
 
-version = os.environ["VERSION"]
+version = os.environ["VERSION"] + "." + os.environ["GITHUB_RUN_NUMBER"]
 
 f = open(".minecraft/config/itlt.cfg", "r", encoding="utf-8")
 f_list = f.readlines()
 for i, line in enumerate(f_list):
     if (line.lstrip().startswith("S:windowDisplayTitle")):
-        f_list[i] = "S:windowDisplayTitle=Herodotus " + version
+        f_list[i] = "    S:windowDisplayTitle=Herodotus " + version
 f.close()
 
 f = open(".minecraft/config/itlt.cfg", "w", encoding="utf-8")
