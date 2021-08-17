84b6e62d2a59de8c44fa53c20e21ccba62da1200
youyihj <youyi580@qq.com>
2021-08-17 10:16:37 +0800

    fix path

--- runners/version.py
+++ runners/version.py
@@ -2,13 +2,13 @@ import os
 
 version = os.environ["VERSION"] + "." + os.environ["GITHUB_RUN_NUMBER"]
 
-f = open(".minecraft/fancymenu/config.txt", "r", encoding="utf-8")
+f = open(".minecraft/config/fancymenu/config.txt", "r", encoding="utf-8")
 f_list = f.readlines()
 for i, line in enumerate(f_list):
     if (line.lstrip().startswith("S:customwindowtitle")):
         f_list[i] = "S:customwindowtitle = 'Herodotus build " + version + "';\n"
 f.close()
 
-f = open(".minecraft/fancymenu/config.txt", "w", encoding="utf-8")
+f = open(".minecraft/config/fancymenu/config.txt", "w", encoding="utf-8")
 f.writelines(f_list)
 f.close()
