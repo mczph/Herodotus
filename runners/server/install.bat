5868285a65f7db44fb1ddd47f8c5645c93f01572
GBLodb <garybluis@outlook.com>
2022-01-02 19:40:22 +0800

    Server auto install and server script final, Close #19, NORUNNER

--- runners/server/install.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-set INSTALL_JAR=forge-1.12.2-14.23.5.2860-installer.jar
-
-:install_files
-echo Installing Forge.
-java -jar %INSTALL_JAR% --installServer > NUL 2>&1
-echo Done!
--- runners/server/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-export INSTALL_JAR= "forge-1.12.2-14.23.5.2860-installer.jar"
-
-echo "Installing Forge."
-java -jar ${INSTALL_JAR} --installServer > /dev/null 2>&1
-echo "Done!"
